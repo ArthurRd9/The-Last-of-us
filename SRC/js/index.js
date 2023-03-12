484e93093494be4405baa75ff900efe97281115a
ArthurRd9 <120611671+ArthurRd9@users.noreply.github.com>
2023-03-12 13:48:19 -0300

    Finalizei o Projeto

---
+++ SRC/js/index.js
@@ -0,0 +1,34 @@
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        
+
+        desativarBotaoSelecionado();
+
+        selecionarBotaoCarrossel(botao);
+
+        esconderImagemAtiva();
+
+        imagens[indice].classList.add('ativa');
+    })
+})
+
+function selecionarBotaoCarrossel(botao) {
+    botao.classList.add('selecionado');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    mostrarImagemDeFundo(imagemAtiva);
+}
+
+function mostrarImagemDeFundo(imagemAtiva) {
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
